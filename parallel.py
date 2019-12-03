54c7ab36d6e0f7406eb187bb57896e81c1a7bcbc
YuPo13 <yuliya.povzlo@gmail.com>
2019-12-03 12:28:07 +0200

    Why is it parallel?

--- parallel.py
+++ parallel.py
@@ -1,2 +1,30 @@
+"""The class created has 3 dimensions and
+provides opportunity to calculate its object's volume"""
+
 class Box:
-    pass
+    """
+    Class created has parameters of
+    length, width, height being input by user
+    """
+
+    def __init__(self, length, width, height):
+        """Define obligatory parameters of Box object"""
+        self.length = length
+        self.width = width
+        self.height = height
+
+    def get_length(self):
+        """Define request for parameter input by user"""
+        return self.length
+
+    def get_width(self):
+        """Define request for parameter input by user"""
+        return self.width
+
+    def get_height(self):
+        """Define request for parameter input by user"""
+        return self.height
+
+    def get_volume(self):
+        """Calculate the volume of Box object"""
+        return self.length * self.width * self.height
