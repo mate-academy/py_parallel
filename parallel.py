aa3a64fa4dd3799e31b9fd7c7b45ff6d500c315a
d1makov <makovskyj.danylo@gmail.com>
2019-12-03 11:53:30 +0200

    final

--- parallel.py
+++ parallel.py
@@ -1,2 +1,47 @@
+"""
+docstring
+"""
+
+
 class Box:
-    pass
+    """
+    class docstring
+    """
+    def __init__(self, length, width, height):
+        """
+
+        :param length:
+        :param width:
+        :param height:
+        """
+        self.length = length
+        self.width = width
+        self.height = height
+
+    def get_length(self):
+        """
+
+        :return:
+        """
+        return self.length
+
+    def get_width(self):
+        """
+
+        :return:
+        """
+        return self.width
+
+    def get_height(self):
+        """
+
+        :return:
+        """
+        return self.height
+
+    def get_volume(self):
+        """
+
+        :return:
+        """
+        return self.length * self.width * self.height
