f83c9723d779a304c601a69231f487c14c8f9929
ValentinaLV <tina.lysenok@gmail.com>
2019-12-03 11:18:54 +0200

    hw12_py_parallel

--- parallel.py
+++ parallel.py
@@ -1,2 +1,42 @@
+"""
+module parallel
+"""
+
+
 class Box:
-    pass
+    """
+    class Box representing the box in accordance with the interface specified in the test file.
+    """
+
+    def __init__(self, length: float, width: float, height: float):
+        self._length = length
+        self._width = width
+        self._height = height
+
+    def get_length(self) -> float:
+        """
+        Return length attribute of the box object.
+        :return: float
+        """
+        return self._length
+
+    def get_width(self) -> float:
+        """
+        Return width attribute of the box object.
+        :return: float
+        """
+        return self._width
+
+    def get_height(self) -> float:
+        """
+        Return height attribute of the box object.
+        :return: float
+        """
+        return self._height
+
+    def get_volume(self) -> float:
+        """
+        Calculate volume of the box object.
+        :return: float
+        """
+        return self._length * self._width * self._height
