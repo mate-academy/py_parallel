2d4389bfc615ecb641150c6c94a0794e3c144ec8
Andrii <andrewsotnikov88@gmail.com>
2019-12-05 20:51:02 +0200

    Volume calculation

--- parallel.py
+++ parallel.py
@@ -1,2 +1,28 @@
+"""Create a class for representing the box
+ in accordance with the interface specified in the test file."""
+
+
 class Box:
-    pass
+    """Class for representing the box"""
+
+    def __init__(self, length: float, width: float, height: float):
+        self.length = length
+        self.width = width
+        self.height = height
+
+    def get_length(self) -> float:
+        """Returns length attribute"""
+        return self.length
+
+    def get_width(self) -> float:
+        """Returns width attribute"""
+        return self.width
+
+    def get_height(self) -> float:
+        """Returns height attribute"""
+        return self.height
+
+    def get_volume(self) -> float:
+        """Returns volume. The volume is the multiplication of the width, height and length."""
+        volume = self.height * self.width * self.length
+        return volume
